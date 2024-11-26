f5c3285e47a439a4a2664728be34029199f5e28d
FAUZAN Wijaya <neacakbar@gmail.com>
2024-11-26 20:29:45 +0700

    add streamlit

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Bidan Ani",
-    page_icon="🏥",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "https://www.extremelycoolapp.com/about"
-    }
+st.title("Sistem Konsultasi Rumah Sakit 98")
+st.sidebar.title("Navigasi")
+
+# Navigasi ke halaman yang berbeda
+page = st.sidebar.radio(
+    "Pilih Halaman",
+    ["Landing Page", "Login Dokter", "Diagnosa", "Lihat Gejala", "Riwayat Konsultasi"]
 )
+
+if page == "Landing Page":
+    st.write("Selamat datang di Rumah Sakit 98!")
+    st.write("Silakan gunakan menu navigasi di samping untuk masuk ke halaman yang diinginkan.")
+elif page == "Login Dokter":
+    from theApp.auth import auth_page
+    auth_page()
+elif page == "Diagnosa":
+    from theApp.diagnosa import diagnosa_page
+    diagnosa_page()
+elif page == "Lihat Gejala":
+    from theApp.lihatGejala import gejala_page
+    gejala_page()
+elif page == "Riwayat Konsultasi":
+    from theApp.history import history_page
+    history_page()
+else:
+    st.write('tidak ada pilihan yang dipilih')
